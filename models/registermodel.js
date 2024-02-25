77dc4527674945d12f006e22117f6f2eb5a0a77e
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-02-25 15:43:35 +0530

    Update registermodel.js

--- models/registermodel.js
+++ models/registermodel.js
@@ -1,4 +1,4 @@
-const mongoose =require("mongoose")
+const mongoose =require("mongoose");
 const registermodel= mongoose.Schema({
     username:{type:String,required:true},
     email:{type:String,required:true},
