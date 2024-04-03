38c87d2d14e106015bb1784c0eb3c0b9d8aa25e1
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-04-03 10:03:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express= require("express")
 const app = express();
-require('dotenv').config();
+require('dotenv').config()
 const cors= require("cors");
 const connection= require("./dbconnection");
 connection();
